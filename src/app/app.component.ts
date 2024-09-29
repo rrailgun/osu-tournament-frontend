516e52b7b55cd3583c92e4aae1fc5bc4246a540d
rrailgun <rrailgun456@gmail.com>
2024-09-29 01:30:49 -0400

    add authentication process to frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpRequestsService } from './services/http-requests.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +10,17 @@ import { HttpRequestsService } from './services/http-requests.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'osu-tournament-frontend';
+  userInformation: any; //should create a models folder and make User, Team models etc.
 
-  constructor(private apiRequests: HttpRequestsService) {
-    apiRequests.registerForTournament();
+  constructor(private apiRequests: HttpRequestsService,
+    private route: ActivatedRoute
+  ) {
+    apiRequests.getSelf().subscribe(res => {
+      this.userInformation = res;
+    })
+  }
+
+  loginOsuOauth():void {
+    this.apiRequests.loginOsuOauth();
   }
 }
--- src/app/services/http-requests.service.ts
+++ src/app/services/http-requests.service.ts
@@ -1,16 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { buildUrl } from 'osu-web.js';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpRequestsService {
+  baseUrl: string = 'http://localhost:3000'
 
   constructor(private http: HttpClient) {}
 
+  public loginOsuOauth(): void {
+    window.location.href=buildUrl.authRequest(35056, 'http://localhost:3000/auth/cb', ['identify']);
+  }
+
+  public getToken(code: string): void {
+    console.log(code)
+    this.http.get('/api/auth/cb?code='+code).subscribe(res => console.log(res))
+  }
+
   public registerForTournament(): boolean {
-    this.http.post('http://localhost:3000/api/tournament/register', {}).subscribe();
-    console.log('request')
+    this.http.post('/api/tournament/register', {}).subscribe();
     return true;
   }
+
+  public getSelf(): Observable<{username: string, id: number}> {
+    return this.http.get<{username: string, id: number}>('/api/getSelf');
+  }
+  
 }
